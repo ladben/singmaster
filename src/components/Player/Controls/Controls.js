28661c5ee75d1a474534da6f99d549b9a4d271be
AzureAD\BenceLadanyi <bence.ladanyi@prefixbox.com>
2023-10-31 19:25:05 +0100

    more architecture change

--- src/components/Player/Controls/Controls.js
+++ src/components/Player/Controls/Controls.js
@@ -3,16 +3,19 @@ import './Controls.css'
 import {ReactComponent as SettingsIcon} from '../../../assets/settings.svg';
 import musicInfo from '../../../assets/lyics/nemTudjaSenki/header.json'
 
+
 function Header() {
 
     
 
     return (
-        <div className='header'>
-            <SettingsIcon className='settings'/>
-            <div className='music-info'>
-                <div className='title'>{musicInfo.title}</div>
-                <div className='artist'>{musicInfo.artist}</div>
+        <div className='controls'>
+            <div className='header'>
+                <SettingsIcon className='settings'/>
+                <div className='music-info'>
+                    <div className='title'>{musicInfo.title}</div>
+                    <div className='artist'>{musicInfo.artist}</div>
+                </div>
             </div>
         </div>
     );
