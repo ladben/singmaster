356c085b987de1c8fa644588a00f0b28d0414578
Ladányi Bence <ladanyi.bence96@gmail.com>
2024-10-08 00:45:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import Player from './components/Player/Player';
+import React, { useContext } from 'react';
+import { AuthContext } from './auth/AuthProvider';
+import { AuthProvider } from './auth/AuthProvider';
+import LoginForm from './LoginForm';
 
 function App() {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
-    <Player />
+    <AuthProvider>
+      {isAuthenticated ? (
+        <>
+          <Player />
+        </>
+      ) : (
+        <div>
+          <LoginForm />
+        </div>
+      )}
+    </AuthProvider>
   );
+
 }
 
 export default App;
