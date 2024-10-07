843634ac4832864442ce4cefc248b5d9712c08d0
Ladányi Bence <ladanyi.bence96@gmail.com>
2024-10-08 00:50:06 +0200

    uninstall auth0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
 import Player from './components/Player/Player';
-import React, { useContext } from 'react';
-import { AuthContext } from './auth/AuthProvider';
-import { AuthProvider } from './auth/AuthProvider';
-import LoginForm from './LoginForm';
 
 function App() {
-  const { isAuthenticated } = useContext(AuthContext);
 
   return (
-    <AuthProvider>
-      {isAuthenticated ? (
-        <>
-          <Player />
-        </>
-      ) : (
-        <div>
-          <LoginForm />
-        </div>
-      )}
-    </AuthProvider>
+    <Player />
   );
 
 }
