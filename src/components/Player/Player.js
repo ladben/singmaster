17fcf1a93063b529769ae2c2dd5dd1ed333dfe0b
AzureAD\BenceLadanyi <bence.ladanyi@prefixbox.com>
2024-10-08 13:54:01 +0200

    nyuszi commit

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -3,11 +3,13 @@ import './Player.css'
 import AudioPlayerUI from './AudioPlayerUI/AudioPlayerUI';
 import Lyrics from './Lyrics/Lyrics';
 import Controls from './Controls/Controls';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import SeekTime from './SeekTime/SeekTime';
 
 function Player () {
     const [currLineTime, setCurrLineTime] = useState(0);
+    const [seekedPercent, setSeekedPercent] = useState(0);
+    const [duration, setDuration] = useState(0);
     const appRef = useRef(null);
 
     window.addEventListener('resize', ()=>{
@@ -16,11 +18,33 @@ function Player () {
         }
     });
 
+    useEffect(() => {
+        const seeker = document.querySelector('.rhap_progress-container');
+        const seekerObserver = new MutationObserver((mutationList) => {
+            for (const mutation of mutationList) {
+                if (mutation.type === 'attributes' && mutation.attributeName === 'aria-valuenow') {
+                    setSeekedPercent(Number(mutation.target.ariaValueNow));
+                }
+            }
+        });
+    
+        seekerObserver.observe(seeker, {
+            attributes: true
+        });
+
+        const audio = document.querySelector('audio');
+        console.log(audio);
+        audio.addEventListener('loadeddata', () => {
+            setDuration(audio.duration);
+        })
+
+    }, []);
+
     return (
         <div className='audio-player' ref={appRef} style={{height: `${window.innerHeight}px`}}>
             <Controls />
             <Lyrics currLineTime={currLineTime}/>
-            <SeekTime />
+            <SeekTime duration={duration} seekedPercent={seekedPercent}/>
             <AudioPlayerUI setCurrLineTime={setCurrLineTime}/>
         </div>
     );
--- src/components/Player/SeekTime/SeekTime.js
+++ src/components/Player/SeekTime/SeekTime.js
@@ -1,8 +1,17 @@
 import './SeekTime.css';
 
-const SeekTime = () => {
+const SeekTime = ({duration, seekedPercent}) => {
+
+    const seekedTime = duration * (seekedPercent / 100);
+
+    let minutes = Math.floor(seekedTime / 60);
+    let seconds = Math.floor(seekedTime - (minutes * 60));
+
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+
     return (
-        <div id="seeked_time">32:11</div>
+        <div id="seeked_time">{minutes}:{seconds}</div>
     );
 }
  
